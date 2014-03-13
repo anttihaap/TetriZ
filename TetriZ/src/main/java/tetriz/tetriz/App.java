0dad9740d4b353700fb72460806b0bf21a04ad7c
Antti Haapaniemi <antti.haapaniemi@helsinki.fi>
2014-03-13 23:16:26 +0200

    NetBeans-projektikansio lisätty

---
+++ TetriZ/src/main/java/tetriz/tetriz/App.java
@@ -0,0 +1,13 @@
+package tetriz.tetriz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
